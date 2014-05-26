e81723f1d346f614d407a098df9ddca5fb3b3b7e
John Doty <jrhdoty@gmail.com>
2014-05-26 13:51:50 -0700

    Updates boxSpec to test new general implementation of box class

--- spec/boxSpec.js
+++ spec/boxSpec.js
@@ -3,11 +3,22 @@
 describe('Box', function () {
   'use strict';
   var box, box2, box3;
+  var point, point1, point2, point3, point4, point5, point6, point7;
 
   beforeEach(function(){
-    box  = new Box(new Point(0, 0), 10, 10);
-    box2 = new Box(new Point(5, 5), 10, 10);
-    box3 = new Box(new Point(11, 11), 10, 10);
+    point  = new Point(0, 0);
+    point1 = new Point(10, 10);
+    point2 = new Point(20, 20);
+    point3 = new Point(15, 15);
+    point4 = new Point(5, 15);
+    point5 = new Point(10, 30);
+    point6 = new Point(21, 21);
+    point7 = new Point(30, 30);
+
+
+    box  = new Box(point, point2);
+    box2 = new Box(point1, point3);
+    box3 = new Box(point6, point7);
   });
 
   it('exists', function () {
@@ -20,45 +31,37 @@ describe('Box', function () {
     expect(box.overlaps).to.be.a('function');
   });
 
-  it('detects an overlap correctly', function(){
-    expect(box.overlaps(box2)).to.equal(true);
+  it('detects that a point is contained correctly', function(){
+    expect(box.containsPoint(point1)).to.equal(true);
+    expect(box.containsPoint(point5)).to.equal(false);
   });
 
-  it('detects a lack of overlap correctly', function(){
+  it('detects an overlap correctly', function(){
+    expect(box.overlaps(box2)).to.equal(true);
     expect(box.overlaps(box3)).to.equal(false);
   });
 
-  it('returns the correct top left quadrant', function(){
-    var b = box.getQuadrant(0);
-    expect(b.vertex.x).to.equal(0);
-    expect(b.vertex.y).to.equal(0);
-    expect(b.width).to.equal(5);
-    expect(b.height).to.equal(5);
-  });
+  it('returns correct quadrants when split', function(){
+    var results = box.split();
+    expect(results[0].low.x).to.equal(0);
+    expect(results[0].low.y).to.equal(0);
+    expect(results[0].high.x).to.equal(10);
+    expect(results[0].high.y).to.equal(10);
 
-  it('returns the correct top right quadrant', function(){
-    var b = box.getQuadrant(1);
-    expect(b.vertex.x).to.equal(5);
-    expect(b.vertex.y).to.equal(0);
-    expect(b.width).to.equal(5);
-    expect(b.height).to.equal(5);
-  });
+    expect(results[1].low.x).to.equal(10);
+    expect(results[1].low.y).to.equal(0);
+    expect(results[1].high.x).to.equal(20);
+    expect(results[1].high.y).to.equal(10);
 
-  it('returns the correct bottom right quadrant', function(){
-    var b = box.getQuadrant(2);
-    expect(b.vertex.x).to.equal(5);
-    expect(b.vertex.y).to.equal(5);
-    expect(b.width).to.equal(5);
-    expect(b.height).to.equal(5);
-  });
+    expect(results[2].low.x).to.equal(10);
+    expect(results[2].low.y).to.equal(10);
+    expect(results[2].high.x).to.equal(20);
+    expect(results[2].high.y).to.equal(20);
 
-  it('returns the correct bottom left quadrant', function(){
-    var b = box.getQuadrant(3);
-    expect(b.vertex.x).to.equal(0);
-    expect(b.vertex.y).to.equal(5);
-    expect(b.width).to.equal(5);
-    expect(b.height).to.equal(5);
+    expect(results[3].low.x).to.equal(0);
+    expect(results[3].low.y).to.equal(10);
+    expect(results[3].high.x).to.equal(10);
+    expect(results[3].high.y).to.equal(20);
   });
-
   // Add more assertions here
 });
